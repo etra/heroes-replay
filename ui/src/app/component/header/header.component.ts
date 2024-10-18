d77bb9fbef4ea7698536eb6ed4b4fa202e0b1fd4
etra <arturas.tutkus@gmail.com>
2024-10-17 23:48:17 -0400

    Adding some progress

--- ui/src/app/component/header/header.component.ts
+++ ui/src/app/component/header/header.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatButton } from '@angular/material/button';
-import { MatFormField } from '@angular/material/form-field';
+
 import { filter } from 'rxjs/operators';
 import { NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButton, MatToolbar, MatFormField, NgIf, RouterModule],
+  imports: [NgIf, RouterModule, MatButtonModule, MatFormFieldModule, MatIconModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- ui/src/app/pages/index/index.component.ts
+++ ui/src/app/pages/index/index.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, NgZone,  } from '@angular/core';
+import { LayoutComponent } from '../../component/layout/layout.component';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule, NgFor } from '@angular/common';
+import { ColorService, Color } from '../../services/color.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [LayoutComponent, MatGridListModule, CommonModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
+  
+  data$: Observable<Color[]> | undefined;
 
+  constructor(
+    private colorService: ColorService,
+    private cd: ChangeDetectorRef, // Inject ChangeDetectorRef
+    private zone: NgZone // Inject NgZone
+  ) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  // loadData(): void {
+  //   this.colorService.getData().subscribe((data: Color[]) => {
+  //     console.log('Data loaded:', data);
+  //     this.colors = [...data];
+  //     console.log('Colors:', this.colors);
+  //   });
+  // }
+
+  loadData(): void {
+    this.data$ = this.colorService.getData();
+    // this.colorService.getData().subscribe((data: Color[]) => {
+    //   console.log('Data loaded:', data);
+    //   this.colors = [...data];
+    //   console.log('Colors:', this.colors);
+    // });
+  }
+
+
+  videos = [
+    { title: 'Video 1', channel: 'Channel 1', thumbnail: 'assets/video1.jpg' },
+    { title: 'Video 2', channel: 'Channel 2', thumbnail: 'assets/video2.jpg' },
+    { title: 'Video 3', channel: 'Channel 3', thumbnail: 'assets/video3.jpg' },
+    { title: 'Video 4', channel: 'Channel 4', thumbnail: 'assets/video4.jpg' },
+    { title: 'Video 5', channel: 'Channel 5', thumbnail: 'assets/video5.jpg' },
+  ];
 }
