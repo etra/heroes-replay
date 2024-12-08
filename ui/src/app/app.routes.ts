d9dfc9d17b63cab0cba14cd6c4ea4cbb353ddcfa
etra <arturas.tutkus@gmail.com>
2024-12-07 23:01:32 -0500

    Adding player creation (json generator)

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { DetailComponent } from './pages/detail/detail.component';
 import { PlayerComponent } from './pages/player/player.component';
-
+import { CreatePlayerComponent } from './pages/create-player/create-player.component';
+import { CreateMatchComponent } from './pages/create-match/create-match.component';
 // import { ResultComponent } from './pages/result/result.component';
 // import { DetailComponent } from './pages/detail/detail.component';
 
@@ -11,4 +12,6 @@ export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'match/:id', component: DetailComponent },
   { path: 'player/:id', component: PlayerComponent },
+  { path: 'create-player', component: CreatePlayerComponent },
+  { path: 'create-match', component: CreateMatchComponent },
 ];
